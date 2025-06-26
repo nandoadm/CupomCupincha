de6fddd2041cb86e753a23db6d30c2a2dc117bf1
admnando <nando.comth@gmail.com>
2025-06-26 10:37:05 -0300

    Configuração de rota login e template login

--- src/main/java/org/backendlogin/frontend/modules/controllers/UserController.java
+++ src/main/java/org/backendlogin/frontend/modules/controllers/UserController.java
@@ -1,4 +1,29 @@
 package org.backendlogin.frontend.modules.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class UserController {
+
+    @GetMapping("/")
+    public String login() {
+        return "users/login";
+    }
+
+    @PostMapping("/signIn")
+    public String signIn(String username, String password) {
+        if (username.equals("admin") && password.equals("123")) {
+            return "users/admin";
+        }
+        return "redirect:/login/";
+    }
+
+//    @GetMapping("/admin")
+//    public String admin() {
+//        return "users/admin";
+//    }
 }
